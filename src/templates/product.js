40f3d14aafdc27908dc8cc809aa4f7add0927964
Allisyn Abrams <allisynabrams@Allisyns-MBP-2.fios-router.home>
2020-11-25 18:59:08 -0500

    size and qty state updating on product page

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,9 +1,24 @@
-import React from "react"
+import React, {useState} from "react"
 import Layout from "../components/layout"
 import { css } from "@emotion/core"
+import { Button, ButtonGroup } from "reactstrap"
 
 const ProductTemplate = ({ pageContext }) => {
   const { product } = pageContext
+  // const {product.variants[i].title} = 
+
+  const [selectedSize, setSelectedSize] = useState(null)
+  const [selectedQty, setSelectedQty] = useState(null)
+
+
+  const handleAddToBag = (e) => {
+    e.preventDefault()
+  }
+  const handleQtyChange = e => {
+    // setSelectedQty({ ...selectedQty, [e.target.name]: e.target.value })
+    setSelectedQty(e.target.value )
+  }
+  console.log("selectedQty", selectedQty)
 
   return (
     <Layout>
@@ -18,8 +33,51 @@ const ProductTemplate = ({ pageContext }) => {
           />
         </div>
       </div>
-      <h1>{product.title}</h1>
+      <h3>{product.title}</h3>
       <div>{product.description}</div>
+
+      {/*--------- TESING ---------*/}
+      <p css={{ color: `red`, fontSize: `14px` }}>TESTING STATE:</p>
+      <p css={{ color: `red`, fontSize: `14px` }}>
+        selected size: {selectedSize}
+      </p>
+      <p css={{ color: `red`, fontSize: `14px` }}>
+        selected qty: {selectedQty}
+      </p>
+      {/*--------- TESING ---------*/}
+
+      <form id="product-form" className="product-add">
+        <div className="sizes">
+          <p>Selected Size: {selectedSize}</p>
+          <ButtonGroup>
+            {product.variants.map(variant => (
+              <div key={variant.id}>
+                <Button
+                  key={variant.id}
+                  name="size"
+                  onClick={() => setSelectedSize(`${variant.title}`)}
+                  active={selectedSize === `${variant.title}`}
+                  css={{ margin: `5px` }}
+                >
+                  {variant.title}
+                </Button>
+              </div>
+            ))}
+          </ButtonGroup>
+        </div>
+
+        <label htmlFor="qty">Quantity</label>
+        <select name="qty" id="qty" onChange={handleQtyChange}>
+          <optgroup label="Qty">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </optgroup>
+        </select>
+
+        <button type="submit">Add to Cart</button>
+      </form>
 
       <pre>{JSON.stringify(product, null, 2)}</pre>
     </Layout>
