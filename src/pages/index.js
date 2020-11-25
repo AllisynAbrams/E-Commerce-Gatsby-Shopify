83ea1560777f6999398ebb9c49354fbe0730eaf6
Allisyn Abrams <allisynabrams@Allisyns-MBP-2.fios-router.home>
2020-11-24 22:40:53 -0500

    testing without allproduct query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import "bootstrap/dist/css/bootstrap.min.css"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -12,8 +10,10 @@ const IndexPage = () => (
     <h1>HI SHOPPERS</h1>
     <h3>Welcome to this awesome e-com site!</h3>
 
-    <h3>The homepage is still a work in progress, but go ahead and test out the
-      navigation links to see some products and details.</h3>
+    <h3>
+      The homepage is still a work in progress, but go ahead and test out the
+      navigation links to see some products and details.
+    </h3>
 
     <p>
       Disclaimer: Please don't actually attempt to order anything because these
--- src/pages/shop-all.js
+++ src/pages/shop-all.js
@@ -1,90 +1,90 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import { node } from "prop-types"
-import { css } from "@emotion/core"
-import Layout from "../components/layout"
+// import React from "react"
+// import { Link, graphql } from "gatsby"
+// import { node } from "prop-types"
+// import { css } from "@emotion/core"
+// import Layout from "../components/layout"
 
-const AllProducts = ({ data }) => {
-  const product = data.allShopifyProduct
-  console.log("this is product: ", product)
-  console.log("this is img", product.nodes[0].images[0].originalSrc)
+// const AllProducts = ({ data }) => {
+//   const product = data.allShopifyProduct
+//   console.log("this is product: ", product)
+//   console.log("this is img", product.nodes[0].images[0].originalSrc)
 
-  let productMap = product.nodes.map((product, i) => (
-    <div key={product.id} className="col-md-4">
-      <Link to={`/product/${product.handle}/`}>
-        <div className="card card-product-grid">
-          <div className="img-wrap">
-            <img src={product.images[0].originalSrc} alt={product.title} />
-          </div>
-          <h3>{product.title}</h3>
-          <div className="price-wrap mt-2">
-            <span className="price">
-              ${product.priceRange.maxVariantPrice.amount}
-            </span>
-          </div>
-          {product.variants.length > 1
-            ? product.variants.map(variant => (
-                <div key={variant.title}>
-                  <p>{variant.title}</p>
-                </div>
-              ))
-            : null}
-        </div>
-      </Link>
-    </div>
-  ))
+//   let productMap = product.nodes.map((product, i) => (
+//     <div key={product.id} className="col-md-4">
+//       <Link to={`/product/${product.handle}/`}>
+//         <div className="card card-product-grid">
+//           <div className="img-wrap">
+//             <img src={product.images[0].originalSrc} alt={product.title} />
+//           </div>
+//           <h3>{product.title}</h3>
+//           <div className="price-wrap mt-2">
+//             <span className="price">
+//               ${product.priceRange.maxVariantPrice.amount}
+//             </span>
+//           </div>
+//           {product.variants.length > 1
+//             ? product.variants.map(variant => (
+//                 <div key={variant.title}>
+//                   <p>{variant.title}</p>
+//                 </div>
+//               ))
+//             : null}
+//         </div>
+//       </Link>
+//     </div>
+//   ))
 
-  return <Layout>{productMap}</Layout>
-}
+//   return <Layout>{productMap}</Layout>
+// }
 
-export const query = graphql`
-  query {
-    allShopifyProduct(filter: { availableForSale: { eq: true } }) {
-      nodes {
-        title
-        shopifyId
-        tags
-        id
-        handle
-        productType
-        images {
-          originalSrc
-        }
-        priceRange {
-          maxVariantPrice {
-            amount
-          }
-        }
-        productType
-        variants {
-          title
-          image {
-            localFile {
-              childImageSharp {
-                original {
-                  width
-                  height
-                  src
-                }
-              }
-            }
-          }
-          title
-          priceV2 {
-            amount
-            currencyCode
-          }
-          sku
-          product {
-            productType
-          }
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query {
+//     allShopifyProduct(filter: { availableForSale: { eq: true } }) {
+//       nodes {
+//         title
+//         shopifyId
+//         tags
+//         id
+//         handle
+//         productType
+//         images {
+//           originalSrc
+//         }
+//         priceRange {
+//           maxVariantPrice {
+//             amount
+//           }
+//         }
+//         productType
+//         variants {
+//           title
+//           image {
+//             localFile {
+//               childImageSharp {
+//                 original {
+//                   width
+//                   height
+//                   src
+//                 }
+//               }
+//             }
+//           }
+//           title
+//           priceV2 {
+//             amount
+//             currencyCode
+//           }
+//           sku
+//           product {
+//             productType
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
 
-export default AllProducts
+// export default AllProducts
 
 // ref:
 // https://dev.to/idiglove/display-shopify-collections-in-your-gatsby-ecommerce-site-2459
