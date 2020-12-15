77f2af82a6180b6956da4c10cbd71480c6ed365d
Allisyn Abrams <allisynabrams@Allisyns-MBP-2.fios-router.home>
2020-12-15 16:52:42 -0500

    line item counter over cart icon

--- src/components/header.js
+++ src/components/header.js
@@ -1,68 +1,78 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useContext } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faShoppingCart,
   faHeart,
   faUser,
 } from "@fortawesome/free-solid-svg-icons"
+import { StoreContext } from "../context/StoreContext"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `lightgrey`,
-      marginBottom: `1.45rem`,
-      lineHeight: `0.3`,
-    }}
-  >
-    <div
-      className="header-container"
+const Header = ({ siteTitle }) => {
+  const {store: { checkout }} = useContext(StoreContext)
+  const lineItemCount = 
+    checkout.lineItems.reduce((accumulator, item) => accumulator + item.quantity, 0)
+    console.log('this is lineItemCount - ', lineItemCount)
+    
+
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `.65rem 1.0875rem`,
+        background: `lightgrey`,
+        marginBottom: `1.45rem`,
+        lineHeight: `0.3`,
       }}
     >
-      <h1 className="sitetitle" style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div
+        className="header-container"
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `.65rem 1.0875rem`,
+        }}
+      >
+        <h1 className="sitetitle" style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
 
-      {/* <i class="fas fa-heart" style={{ fontSize: `30px`, color: `black` }}></i> */}
+        {/* <i class="fas fa-heart" style={{ fontSize: `30px`, color: `black` }}></i> */}
 
-      <div className="dashboard-container">
-        <FontAwesomeIcon
-          icon={faHeart}
-          size="2x"
-          className="fa-icon"
-        ></FontAwesomeIcon>
-
-        <FontAwesomeIcon
-          icon={faUser}
-          size="2x"
-          className="fa-icon"
-        ></FontAwesomeIcon>
-
-        <Link to="/cart/">
+        <div className="dashboard-container">
           <FontAwesomeIcon
-            icon={faShoppingCart}
+            icon={faHeart}
             size="2x"
-            color="black"
             className="fa-icon"
           ></FontAwesomeIcon>
-        </Link>
+
+          <FontAwesomeIcon
+            icon={faUser}
+            size="2x"
+            className="fa-icon"
+          ></FontAwesomeIcon>
+
+          <Link to="/cart/">
+            <FontAwesomeIcon
+              icon={faShoppingCart}
+              size="2x"
+              color="black"
+              className="fa-icon"
+            ></FontAwesomeIcon>
+            <span className="line-item-count">{lineItemCount}</span>
+          </Link>
+        </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
